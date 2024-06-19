ba76473ef266279990bc0fb4c2d642d1612b568f
mainrdsniper90 <barretosanchez6@gmail.com>
2024-06-19 18:10:57 -0500

    Se crea la clase conexion, junto a ella las variables de clase

---
+++ conexcion.py
@@ -0,0 +1,8 @@
+class Conexion:  # Creacion de clase Conexion
+    _DATABASE = 'test_db'  # se crean la variables de clase
+    _USERNAME = 'admin'
+    _PASSWORD = '1234'
+    _DB_PORT = '5432'
+    _HOST = 'localhost'
+    _conexion = None
+    _curson = None
