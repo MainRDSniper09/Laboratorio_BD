87efa38b4ad03dae4f31e22036b99a7c3a3eb94a
mainrdsniper90 <barretosanchez6@gmail.com>
2024-06-19 18:21:13 -0500

    Importamos la libreria psycopg2
    Creamos la clase obtener conexion la cual nos permite crear un objeto tipo conexion, el cual hara la respectiva conexion a la BD

--- conexcion.py
+++ conexcion.py
@@ -1,3 +1,6 @@
+from logger_base import log  # Se importa la clase logger_base
+import psycopg2 as db  # Se importa libreria de psycopg2 para manejar la db
+
 class Conexion:  # Creacion de clase Conexion
     _DATABASE = 'test_db'  # se crean la variables de clase
     _USERNAME = 'admin'
@@ -6,3 +9,17 @@ class Conexion:  # Creacion de clase Conexion
     _HOST = 'localhost'
     _conexion = None
     _curson = None
+
+    @classmethod
+    def obtener_conexion(cls):  # Creamos la clase obtener conexion la cual nos permite crear un objeto tipo conexion, el cual hara la respectiva conexion a la BD
+        if cls._conexion is None:  # Validamos si el objeto esta vacio
+            try:
+                cls._conexion = db.connect(host=cls._HOST,  # Le asignamos al objeto conexion los parametros para realizar la conexion correctamente
+                                           user=cls._USERNAME,
+                                           password=cls._PASSWORD,
+                                           port=cls._DB_PORT,
+                                           database=cls._DATABASE)
+                log.debug(f'Conexion exitosa: {cls._conexion}')  # Mandamos a imprimir un mensaje de exito
+                return cls._conexion  # Retornamos el objeto conexion
+            except Exception as e:
+                log.debug(f'Ocurrio una excepcion {e}')  # En caso tal de que algo este mal, se imprime el error
