d8e03a9b0116c94a52e5573f4dbbe3baf00796c9
mainrdsniper90 <barretosanchez6@gmail.com>
2024-06-19 20:10:00 -0500

    Se importa las dos clases, tanto de conexion como de persona
    Se crea el metodo de clase seleccionar en donde hacemos uso de la sentencia de sql para seleccionar los elementos
    
    Luego creamos un registro en donde va a recuperar todos los registros de nuestra tabla
    
    Creamos una lista para que guarde los registros ahí
    
    Creamos un for para que itere el contenido de los registros obtenido anteriormente y lo guardamos en una variable registro
    
    Creamos un objeto persona y lo volvemos una lista, para que guarde los registros en indices
    
    Creamos una variable llamada personas en donde haciendo uso de append() agregamos los registros anteriormente recuperados a la lista creada
    
    Finalmente retornamos nuestra lista personas

--- persona_dao.py
+++ persona_dao.py
@@ -1,3 +1,7 @@
+from conexion import Conexion
+from persona import Persona
+
+
 class PersonaDAO:  # Se crea la el patron de diseño dao, para la conexion de bases de datos
     '''
     DAO (Data Access Object)
@@ -10,3 +14,13 @@ class PersonaDAO:  # Se crea la el patron de diseño dao, para la conexion de ba
     _ACTUALIZAR = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
     _ELIMINAR = 'DELETE FROM persona WHERE id_persona=%s'
 
+    @classmethod  # Creamos el metodo seleccionar en un metodo de clase
+    def seleccionar(cls):
+        with Conexion.obtener_cursor() as cursor:  # Hacemos uso del with para no tener que hacer commits y rollback manuales
+            cursor.execute(cls._SELECCIONAR)  # Usamos el metodo de ejecutar y dentro le pasamos el parametro de cls._SELECCIONAR, el cual tiene la sentencia sql
+            registros = cursor.fetchall()  # Decimos que recupere todos los elementos de nuestro registro
+            personas = []  # Creamos una lista para agregar nuestros registros
+            for registro in registros:  # Usamos un for para iterar los registros obtenidos anteriormente
+                persona = Persona(registro[0], registro[1], registro[2], registro[3])  # Creamos un objeto persona y lo volvemos una lista el cual contrenda los registros y los asignara a su respectivo indice siendo el indice[0] el nombre y asi sucesivamente
+                personas.append(persona) # usamos la operacion de anexar los registros a la lista a una variable llamada personas la cual es una lista
+            return personas  # Pedimos que nos retorne esta lista de personas
