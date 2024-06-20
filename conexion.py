864bb4f025aabd72144961982cdcef292d5436a9
mainrdsniper90 <barretosanchez6@gmail.com>
2024-06-20 10:59:51 -0500

    Creamos el metodo de clase obtener Pool el cual se encargar de crear el objeto pool el cual nos permitira realizar la debida pool de conexiones

--- conexion.py
+++ conexion.py
@@ -1,3 +1,8 @@
+from psycopg2 import pool  # Se importa la clase pool
+
+from logger_base import log
+
+
 class Conexion:  # Creacion de clase Conexion
     _DATABASE = 'test_db'  # se crean la variables de clase
     _USERNAME = 'admin'
@@ -7,6 +12,20 @@ class Conexion:  # Creacion de clase Conexion
     _MIN_CON = 1  # Se crea variable minimo de conexiones
     _MAX_CON = 5  # Se crea variable maximo de conexiones
     _pool = None  # Se crea variable pool para que reciba las diferentes conexiones
+
+    @classmethod  # Creamos el metodo de clase obtenerPool
+    def obtenerPool(cls):
+        if cls._pool is None:  # Preguntamos si no se ha creado un pool
+            try:  # Hcemos un try
+                cls._pool = pool.SimpleConnectionPool(cls._MIN_CON, cls._MAX_CON,
+                                                      # Usando la libreria de psycopg2 le asignamos a nuestra variable de pool el objeto pool y usamos su metodo simpleConnectionPool para realizar la conexion
+                                                      host=cls._HOST, user=cls._USERNAME,
+                                                      password=cls._PASSWORD, port=cls._DB_PORT,
+                                                      database=cls._DATABASE)
+                log.info(f'Creacion del pool exitosa {cls._pool}')
+                return cls._pool
+            except Exception as e:
+                log.error(f'Ocurrio un error al obtener el pool {e}')
 
     @classmethod
     def obtenerConexion(cls):  # Creamos la clase obtener conexion la cual nos permite crear un objeto tipo conexion, el cual hara la respectiva conexion a la BD
