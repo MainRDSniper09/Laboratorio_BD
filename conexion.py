7ceb87f112c59786d967a74542486bc7ce987287
mainrdsniper90 <barretosanchez6@gmail.com>
2024-06-20 10:52:56 -0500

    Se crean las variables minimo conexiones, maximo conexiones y pool

--- conexion.py
+++ conexion.py
@@ -4,7 +4,9 @@ class Conexion:  # Creacion de clase Conexion
     _PASSWORD = '1234'
     _DB_PORT = '5432'
     _HOST = 'localhost'
-
+    _MIN_CON = 1  # Se crea variable minimo de conexiones
+    _MAX_CON = 5  # Se crea variable maximo de conexiones
+    _pool = None  # Se crea variable pool para que reciba las diferentes conexiones
 
     @classmethod
     def obtenerConexion(cls):  # Creamos la clase obtener conexion la cual nos permite crear un objeto tipo conexion, el cual hara la respectiva conexion a la BD
